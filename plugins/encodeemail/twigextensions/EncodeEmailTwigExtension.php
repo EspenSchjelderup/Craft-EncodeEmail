1ccbcd59591371dd23a150d43af7852ec8fc7631
EspenSchjelderup <es@edir.no>
2013-11-22 00:44:22 +0100

    Some replacements, Added CSS Codedirection, Refined ROT13
    * Removed "encode" (duplicate of rot13, instead of creating duplicate functions, maybe it's a better idea to separate internal functions from the actual Twig "filter-name"?)
    
    * Renamed functions (The Twig "filter-name" might/should probably be simplified)
    
    * Refined ROT13 method to include "non-hyperlinked" emails.
    
    * Replaced 'entities method' with 'CSS code direction method' based on various articles suggesting that this CSS method results in less "spam by bots" than using entities. One of the sources/articles: http://techblog.tilllate.com/2008/07/20/ten-methods-to-obfuscate-e-mail-addresses-compared/
    
    * ISSUE: In certain cases, the "document.write" inserted with the ROT13 function causes other code/js to break (I've experienced a case were jQuery "tabbed content" breaks due to the inserted "document.write" js). The CSS code direction represents a good alternate method for "fragile" situations where the code might break.
    
    Note:
    These changes are mainly commited due to specific needs in several sites that are currently being developed, therefore this is only to be regarded as "input" & suggestions to the existing plugin from Barrel Strength Design.

--- plugins/encodeemail/twigextensions/EncodeEmailTwigExtension.php
+++ plugins/encodeemail/twigextensions/EncodeEmailTwigExtension.php
@@ -1,103 +1,88 @@
 <?php
-
 namespace Craft;
 
 use Twig_Extension;
 use Twig_Filter_Method;
 
-class EncodeEmailTwigExtension extends Twig_Extension
-{
-    public function getName()
-    {
-        return 'Encode Email';
-    }
+class EncodeEmailTwigExtension extends Twig_Extension {
+	public function getName() {
+		return 'Encode Email';
+	}
 
-    public function getFilters()
-    {
-        // @TODO - explore the markdown class in Yii.  
-        // It may also have several filters for this type of use case
-        return array(
-            'encode'    => new Twig_Filter_Method($this, 'encode', array('is_safe' => array('html'))),
-            'rot13'     => new Twig_Filter_Method($this, 'rot13', array('is_safe' => array('html'))),
-            'entities'  => new Twig_Filter_Method($this, 'entities', array('is_safe' => array('html')))
-        );
-    }
+	public function getFilters() {
+		return array(
+			'css_obfuscate' => new Twig_Filter_Method($this, 'css_obfuscate', array('is_safe' => array('html'))),
+			'rot13_obfuscate' => new Twig_Filter_Method($this, 'rot13_obfuscate', array('is_safe' => array('html'))),
+		);
+	}
 
-    /**
-     * Encode string using Rot13.  This function does the same as the Rot13 function
-     * and only exists as it may be easier for some folks to remember 'encode'
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          Returns Rot13 encoded string
-     */
-    public function encode($string)
-    {
-		$pattern = "~<a .*?href=[\'|\"]mailto:(.*?)[\'|\"].*?>.*?</a>~";
-		preg_match_all( $pattern, $string, $matches );
+	/**
+	 * Obfuscate emails using CSS codedirection
+	 * 
+	 * @param	string	Input ($string) to check for email adresses
+	 * @return	mixed	Return a string with all emails obfuscated
+	 */
+	public function css_obfuscate($string) {
+		$pattern1 = '#<a.+?href="mailto:(.*?)".+?</a>#';
+		$string = preg_replace( $pattern1, "$1", $string ); // remove hyperlinks from emails that has a hyperlink
+		$pattern2 = "/[A-Za-z0-9_-]+@[A-Za-z0-9_-]+\.([A-Za-z0-9_-][A-Za-z0-9_]+)/";
+		preg_match_all( $pattern2, $string, $matches ); // finds all email adresses (now without hyperlinks)
 		foreach( $matches[0] as $email ) {
 			$email_to_replace = "~$email~";
-			$string = preg_replace( $email_to_replace, $this->_encodeStringRot13($email), $string );
+			$string = preg_replace( $email_to_replace, $this->_obfuscateEmailCss($email), $string );
 		}
 		return $string;
-    }
+	}
 
-    /**
-     * Encode string using Rot13.
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          Returns Rot13 encoded string
-     */
-    public function rot13($string)
-    {
-		$pattern = "~<a .*?href=[\'|\"]mailto:(.*?)[\'|\"].*?>.*?</a>~";
-		preg_match_all( $pattern, $string, $matches );
-		foreach( $matches[0] as $email ) {
+	/**
+	 * Obfuscate emails using Rot13 with javascript inserts
+	 * 
+	 * @param	string	Input ($string) to check for email adresses
+	 * @return	mixed	Return a string with all emails obfuscated
+	 */
+	public function rot13_obfuscate($string) {
+		$pattern1 = '#<a.+?href="mailto:(.*?)".+?</a>#'; // search for all "hyperlinked" emails
+		$pattern2 = '/[A-Za-z0-9_-]+@[A-Za-z0-9_-]+\.([A-Za-z0-9_-][A-Za-z0-9_]+)/'; // search for all "non-hyperlinked" emails
+		preg_match_all( $pattern1, $string, $matches1 );
+		preg_match_all( $pattern2, $string, $matches2 );
+		foreach( $matches1[0] as $email ) {
 			$email_to_replace = "~$email~";
-			$string = preg_replace( $email_to_replace, $this->_encodeStringRot13($email), $string );
+			$string = preg_replace( $email_to_replace, $this->_obfuscateEmailRot13($email), $string ); // obfuscates all "non-hyperlinked" emails
+		}
+		foreach( $matches2[0] as $email ) {
+			$email_to_replace = "~$email~";
+			$string = preg_replace( $email_to_replace, $this->_obfuscateEmailRot13($email), $string ); // obfuscates all "hyperlinked" emails
 		}
 		return $string;
-    }
+	}
 
-    /**
-     * Encode string using HTML Entities.
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          Returns string encoded as HTML Entities
-     */
-    public function entities($string) 
-    {
-        return $this->_encodeHtmlEntities($string);
-    }
+	/**
+	 * Returns an email string using CSS codedirection method
+	 * http://techblog.tilllate.com/2008/07/20/ten-methods-to-obfuscate-e-mail-addresses-compared/
+	 * 
+	 * @param	string	$email to be obfuscated
+	 * @return	mixed	Obfuscated email wrapped with text-direction-span
+	 */
+	private function _obfuscateEmailCss($email) {
+		$obfuscated_email = '<span class="obf" style="unicode-bidi:bidi-override; direction: rtl;">'.strrev($email).'</span>'; 
+		return $obfuscated_email;
+	}
 
-    /**
-     * Returns a rot13 encrypted string as well as a JavaScript decoder function.
-     * http://snipplr.com/view/6037/
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          An encoded string and javascript decoder function
-     */
-    private function _encodeStringRot13($string) {
-		$encrypted_email = null;
+	/**
+	 * Returns a rot13 encrypted string without javascript decoder
+	 * http://snipplr.com/view/6037/
+	 * 
+	 * @param	string	$email be obfuscated
+	 * @return	mixed	Obfuscated email and javascript decoder function
+	 */
+	private function _obfuscateEmailRot13($email) {
+		$obfuscated_email = null;
 		$length = strlen( $email );
 		for( $i = 0; $i < $length; $i++ ) {
-			$encrypted_email .= str_replace( '"', '\"', str_rot13( $email[$i] ) );
-			$return = '<script type="text/javascript">document.write("' . $encrypted_email . '".replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));</script>';
-		}		
+			$obfuscated_email .= str_replace( '"', '\"', str_rot13( $email[$i] ) );
+			$return = '<script type="text/javascript">document.write("' . $obfuscated_email . '".replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));</script>';
+		}
 		return $return;
-    }
-
-    /**
-     * Returns a string converted to html entities
-     * http://goo.gl/LPhtJ
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          Returns a string converted to html entities
-     */
-    private function _encodeHtmlEntities($string)
-    {
-        $string = mb_convert_encoding($string , 'UTF-32', 'UTF-8');
-        $t = unpack("N*", $string);
-        $t = array_map(function($n) { return "&#$n;"; }, $t);
-        return implode("", $t);
-    }
+	} 
 }
+?>
